b09dc14f8b97ee99e20067444339c7d9ccb927f9
Ashwani Pandey <43845224+Akp6528@users.noreply.github.com>
2018-10-23 22:37:16 +0530

    Range

---
+++ range.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+ 
+class N {
+
+	public static void main(String[] args) {
+	 
+   
+Scanner s = new Scanner(System.in);
+	    
+	    // Get L and R from the input
+
+   int L = s.nextInt();
+		
+   int R = s.nextInt();
+   
+   for(int i=L;i<=R;i++)
+        
+          
+   System.out.println(i +" ");
+         
+   System.out.println();
+ // Write here the logic to print all integers between L and R
+      
+   s.close();
+	
+}
+
+}
